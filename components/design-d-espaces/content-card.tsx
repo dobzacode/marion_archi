bcbb6bdb5f0dc34bde3b870ff9a2ad1955f8445a
dobzacode <corentin.kittel@gmail.com>
2023-12-08 14:17:07 +0100

    add exit animation and animate presence

--- components/design-d-espaces/content-card.tsx
+++ components/design-d-espaces/content-card.tsx
@@ -1,54 +1,66 @@
-'use client';
-
 import { motion } from 'framer-motion';
 import Image from 'next/image';
+import React, { useMemo } from 'react';
 import H3 from '../ui/text/h3';
-import { Project } from './content-section';
 
-interface ContentCardProps extends Project {
+interface ContentCardProps {
   actualType: string;
   index: number;
+  project_name: string;
+  onClick: Function;
+  arrLength: number;
 }
 
-export default function ContentCard({
-  project_name,
-  information,
-  picture,
-  index,
-  actualType
-}: ContentCardProps) {
-  const variants = {
-    hidden: { y: -1000 },
-    visible: (i: number) => ({
-      y: 0,
-      transition: {
-        delay: i * 0.115
-      }
-    })
-  };
+const ContentCard: React.FC<ContentCardProps> = React.memo(
+  ({ project_name, index, actualType, onClick, arrLength }: ContentCardProps) => {
+    const variants = useMemo(
+      () => ({
+        hidden: { y: -1000 },
+        visible: (i: number) => ({
+          y: 0,
+          transition: {
+            delay: i * 0.115
+          }
+        }),
+        exit: (i: number) => ({
+          y: -1000,
+          transition: {
+            delay: (arrLength - i - 1) * 0.115
+          }
+        })
+      }),
+      [arrLength]
+    );
 
-  return (
-    <li className="relative h-full w-1/5 overflow-hidden">
-      <motion.button
-        custom={index}
-        initial="hidden"
-        animate="visible"
-        variants={variants}
-        className=" group relative flex aspect-square w-full cursor-pointer items-center justify-center border border-primary90 border-opacity-10 grayscale duration-slow hover:grayscale-0"
-      >
-        <H3 textType={'heading'} className="relative z-10 px-small text-center text-primary1">
-          <strong className="before:inset-y-1/2 before:h-[100%] before:w-[calc(100%_+_40px)] before:-translate-x-2 before:-translate-y-1/2 before:rotate-3 before:scale-x-0 before:bg-primary90 before:duration-slow group-hover:before:scale-x-100">
-            {project_name.toUpperCase()}
-          </strong>
-        </H3>
-        <div className="absolute h-full w-full bg-primary90 bg-opacity-30 duration-slow group-hover:bg-primary20 group-hover:bg-opacity-0"></div>
-        <Image
-          className="-z-10 object-cover"
-          fill
-          alt={`${project_name} picture`}
-          src={`/assets/${actualType}/${project_name}/banner-${project_name}.jpg`}
-        ></Image>
-      </motion.button>
-    </li>
-  );
-}
+    return (
+      <li className="relative h-full w-1/5 overflow-hidden">
+        <motion.button
+          exit="exit"
+          onClick={() => onClick()}
+          custom={index}
+          initial="hidden"
+          animate="visible"
+          variants={variants}
+          className="group relative flex aspect-square w-full cursor-pointer items-center justify-center border border-primary90 border-opacity-10 grayscale duration-slow hover:grayscale-0"
+        >
+          <H3 textType={'heading'} className="relative z-10 px-small text-center text-primary1">
+            <strong className="before:inset-y-1/2 before:h-[100%] before:w-[calc(100%_+_40px)] before:-translate-x-2 before:-translate-y-1/2 before:rotate-3 before:scale-x-0 before:bg-primary90 before:duration-slow group-hover:before:scale-x-100">
+              {project_name.toUpperCase()}
+            </strong>
+          </H3>
+          <div className="absolute h-full w-full bg-primary90 bg-opacity-30 duration-slow group-hover:bg-primary20 group-hover:bg-opacity-0"></div>
+          <Image
+            className="-z-10 object-cover"
+            fill
+            alt={`${project_name} picture`}
+            src={`/assets/${actualType}/${project_name}/banner-${project_name}.jpg`}
+          ></Image>
+        </motion.button>
+      </li>
+    );
+  }
+);
+
+ContentCard.displayName = 'ContentCard';
+
+export default ContentCard;
--- components/design-d-espaces/content-section.tsx
+++ components/design-d-espaces/content-section.tsx
@@ -1,12 +1,13 @@
 'use client';
 
-import { usePathname, useSearchParams } from 'next/navigation';
-import { useEffect, useState } from 'react';
+import { usePathname, useRouter, useSearchParams } from 'next/navigation';
+import { useEffect, useMemo, useState } from 'react';
 
 import architecture from '@/public/design-d-espaces/architecture.json';
 import architecture_interieurs from '@/public/design-d-espaces/architecture_interieurs.json';
 import architecture_retail from '@/public/design-d-espaces/architecture_retail.json';
 import architecture_tertiaire from '@/public/design-d-espaces/architecture_tertiaire.json';
+import { AnimatePresence } from 'framer-motion';
 import { v4 } from 'uuid';
 import ContentCard from './content-card';
 
@@ -43,22 +44,38 @@ export default function ContentSection() {
     setActualType(typeFromParams ? typeFromParams : 'architecture_interieurs');
   }, [pathname, searchParams]);
 
-  // Récupérez le tableau de projets en fonction du type actuel
-  const projectsToShow: Project[] = projectTypes[actualType] || [];
+  // Utilisation de useMemo pour éviter le recalcul de projectsToShow
+  const projectsToShow: Project[] | undefined = useMemo(() => {
+    if (projectTypes[actualType]) {
+      return projectTypes[actualType];
+    }
+
+    return undefined; // Utiliser undefined au lieu de projectsToShow initial
+  }, [actualType]);
+
+  const router = useRouter();
+
+  const handleButtonClick = (project_name: string) => {
+    router.push(`${pathname}?type=${actualType}&project=${project_name}`);
+  };
+
+  // Utiliser React.memo pour éviter le re-render inutile du composant
 
   return (
     <section>
-      <ul className="flex flex-wrap overflow-hidden">
-        {projectsToShow.map((project, index) => {
-          return (
+      <ul className="overflow flex flex-wrap">
+        <AnimatePresence mode="wait">
+          {projectsToShow?.map((project, index) => (
             <ContentCard
+              onClick={() => handleButtonClick(project.project_name)}
+              arrLength={projectsToShow.length}
               index={index}
-              {...project}
+              project_name={project.project_name}
               actualType={actualType}
-              key={v4()}
-            ></ContentCard>
-          );
-        })}
+              key={`${project.project_name}${v4()}`}
+            />
+          ))}
+        </AnimatePresence>
       </ul>
     </section>
   );

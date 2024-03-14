67481dc70cb2ed022aff19da2f997b8f1d569efa
dobzacode <corentin.kittel@gmail.com>
2024-03-14 17:08:40 +0100

    add favicon

--- components/ui/footer/footer.tsx
+++ components/ui/footer/footer.tsx
@@ -6,7 +6,7 @@ export default function Footer({ className }: { className?: string }) {
   return (
     <footer
       className={cn(
-        'caption absolute  bottom-small  z-50 flex w-full items-center justify-center gap-extra-small  mobile-large:gap-medium',
+        'caption slideInFromBottom  absolute bottom-small z-50  flex w-full items-center justify-center gap-extra-small opacity-0 animation-delay-[4s]  mobile-large:gap-medium',
         className
       )}
     >
